8a37c956523a93de5e9acb5253a8f5f8367e1c4c
Joe DiNoto <47672912+joedinoto@users.noreply.github.com>
2019-03-22 14:41:12 -0400

    output
    Included sample output.

--- art50.R
+++ art50.R
@@ -19,3 +19,43 @@ mp_number <- jsonData$data$attributes$signatures_by_constituency$signature_count
 mp_table <- as.data.frame(cbind(mp,mp_number))
 head(mp_table[order(-mp_number),]) #the negative means descending
 DownloadTime
+
+##########
+# output # 
+##########
+
+                       y       x
+12             Australia    8074
+13               Austria    1638
+19               Belgium    3022
+39                Canada    5201
+55               Denmark    1635
+70                France   26523
+75               Germany   11747
+77             Gibraltar    2727
+78                Greece    1010
+88             Hong Kong    1594
+95               Ireland    5141
+98                 Italy    3286
+117           Luxembourg    1080
+141          Netherlands    5562
+143          New Zealand    2854
+149               Norway    1038
+160             Portugal    1628
+175            Singapore    1160
+184                Spain   14750
+195               Sweden    2387
+196          Switzerland    3536
+215 United Arab Emirates    1233
+216       United Kingdom 3371918
+217        United States   12858
+
+                        mp mp_number
+126  Thangam Debbonaire MP     23087
+294      Catherine West MP     19774
+122      Caroline Lucas MP     17947
+142     Daniel Zeichner MP     17866
+262 Rt Hon Diane Abbott MP     17480
+292 Rt Hon Keir Starmer MP     17389
+
+[1] "Fri Mar 22 14:24:59 2019"
